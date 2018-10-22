634eb292ead20c855e2af57b60ff8cef98596514
Denys Lysianskyi <denys.lysianslkyi@gmail.com>
2018-10-22 10:48:33 +0300

    clean code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'teo-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   public title: string = 'Hot Weather Widget';
